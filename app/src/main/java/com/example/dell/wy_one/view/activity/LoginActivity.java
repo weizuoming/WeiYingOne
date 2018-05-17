f76742140aecb8d722bcd5010d51d39a76b18ad5
Your Name <13230024657@163.com>
2018-05-17 10:19:35 +0800

    kuangjia2

--- app/src/main/java/com/example/dell/wy_one/view/activity/LoginActivity.java
+++ app/src/main/java/com/example/dell/wy_one/view/activity/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.example.dell.wy_one;
+package com.example.dell.wy_one.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.dell.wy_one.R;
 
 public class LoginActivity extends AppCompatActivity {
 
